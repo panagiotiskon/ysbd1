60fc66640fb577a6567c2afda7db327c81062bf2
Kiriak01 <kyria@hotmail.gr>
2021-12-14 15:50:05 +0200

    <newnewnew>

--- code/src/hash_file.c
+++ code/src/hash_file.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+int * file; 
 #include <string.h>
-int * file;          ///////////// auto ?????
 #include "bf.h"
 #include "hash_file.h"
 #define MAX_OPEN_FILES 20
@@ -16,27 +16,20 @@ int * file;          ///////////// auto ?????
   }                         \
 }                            \
 
-typedef struct node{
-  Record r;
-  struct node * next;
-  int depth;
-} bucket;
+
 typedef struct node1{
-  bucket * b;
+  BF_Block * b; 
   int id;
   struct node1* next;
 } directory;       
-void push(bucket * head, Record r,int d);
-Record pop(bucket ** head);
-Record remove_last(bucket * head);
-Record remove_by_index(bucket ** head, int n);
-void pushd(directory * head, bucket * b,int i);
-///////////////directory dir;
-FILE * fp=NULL; 
+
+void pushd(directory * head, BF_Block * b,int i);
+
+
+
 HT_ErrorCode HT_Init() {
-  file=malloc(MAX_OPEN_FILES*sizeof(int)); ////na arxikopoihsw ton pinaka me ta files edw o pinakas twra exei mono pointers se files 
-//g  FILE files[MAX_OPEN_FILES]; 
-  
+  file = malloc(MAX_OPEN_FILES*sizeof(int*)); 
+
   return HT_OK;
 }
 
@@ -44,13 +37,17 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
   directory dir; 
   printf("Created empty directory.\n");                 ///////dhmioyrgia directory mesa sthn create index kai oxi global 
   int i;
-  BF_Block *block;
-  BF_Block_Init(&block); 
+  int fp=1; 
+ 
   for(i=0;i<depth*2;i++){ 
-    //BF_AllocateBlock()      //dhmiourgw directory me 4 kelia afou global depth=2 me pointers NULL afou buckets adeia
-    pushd(&dir,NULL,i);
+    BF_Block * block; 
+    BF_AllocateBlock(fp,block);      //dhmiourgw directory me 4 kelia afou global depth=2 me pointers NULL afou BF_Blocks adeia
+    pushd(&dir,block,i); 
   }
-  BF_CreateFile(filename); 
+  BF_CreateFile(filename);
+  int * blocksnum;  
+  BF_GetBlockCounter(fp,blocksnum);
+  printf("blocks num %d\n", blocksnum); 
   
   
   return HT_OK;
@@ -58,7 +55,7 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
 
 HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc){
   
-  //BF_CreateFile(fileName); 
+  //Bf_openfile(fileName); 
 }
 
 HT_ErrorCode HT_CloseFile(int indexDesc) {        //otan ena file kleinei prepei na vgainei kai apo ton pinaka me ta files kai na apodesmeyetai o xwros toy
@@ -76,111 +73,20 @@ HT_ErrorCode HT_PrintAllEntries(int indexDesc, int *id) {
   //insert code here
   return HT_OK;
 }
-void push(bucket * head, Record r,int d) {
-    bucket * current = head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-    /* now we can add a new variable */
-    current->next = (bucket *) malloc(sizeof(bucket));
-    current->next->r = r;
-    current->next->depth=d;
-    current->next->next = NULL;
-  }
-  Record pop(bucket ** head) {
-    Record retval;
-    bucket * next_node = NULL;
-
-    if (*head == NULL) {
-        printf("poping from empty list");
-    }
-
-    next_node = (*head)->next;
-    retval = (*head)->r;
-    free(*head);
-    *head = next_node;
-
-    return retval;
-  }
-  Record remove_last(bucket * head) {
-    Record retval;
-    /* if there is only one item in the list, remove it */
-    if (head->next == NULL) {
-        retval = head->r;
-        free(head);
-        return retval;
-    }
-
-    /* get to the second to last node in the list */
-    bucket * current = head;
-    while (current->next->next != NULL) {
-        current = current->next;
-    }
-
-    /* now current points to the second to last item of the list, so let's remove current->next */
-    retval = current->next->r;
-    free(current->next);
-    current->next = NULL;
-    return retval;
-
-  }
-  Record remove_by_index(bucket ** head, int n) {
-    int i = 0;
-    Record retval;
-    bucket * current = *head;
-    bucket * temp_node = NULL;
-
-    if (n == 0) {
-        return pop(head);
-    }
-
-    for (i = 0; i < n-1; i++) {
-        if (current->next == NULL) {
-            printf("cant pop");
-        }
-        current = current->next;
-    }
-
-    if (current->next == NULL) {
-        printf("cant pop");
-    }
-
-    temp_node = current->next;
-    retval = temp_node->r;
-    current->next = temp_node->next;
-    free(temp_node);
-
-    return retval;
-  }
-
-
-
-
-
-
-
-
 
   //////////////////////////// directories
 
 
-
-
-
-
-
-
-
-  void pushd(directory * head, bucket * b,int i) {
+  void pushd(directory * head, BF_Block * b,int i) {
     directory * current = head;
     while (current->next != NULL) {
         current = current->next;
     }
 
-    /* now we can add a new variable */
+    // now we can add a new variable 
     current->next = (directory *) malloc(sizeof(directory));
     current->next->b = b;
     current->next->id=i;
     current->next->next = NULL;
   }
-
+   
